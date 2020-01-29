95cd1c98157f5341c122094838f1e8abf8286b2a
newway <newway.cnp@gmail.com>
2020-01-29 22:34:15 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './styles/app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="text-red-700">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React ogren
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
